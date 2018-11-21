1ed5c5fb3ec4d28ff71f4dd678a26e4a33e65ba4
Neeha <neeha2311@gmail.com>
2018-11-20 22:40:52 -0600

    attention model

--- model_attention.py
+++ model_attention.py
@@ -3,7 +3,8 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.python.layers.core import Dense
-
+rnn_size_encoder = 256
+rnn_size_decoder = 256
 def make_rnn_cell(rnn_size):
     cell = tf.nn.rnn_cell.LSTMCell(rnn_size)
     cell = tf.nn.rnn_cell.DropoutWrapper(cell,
@@ -27,7 +28,7 @@ def make_attention_cell(dec_cell, rnn_size, enc_output, lengths):
                                                output_attention=False)
 
 def blstm(inputs,
-          seq_len,
+          input_sequence_length,
           n_hidden,
           scope=None,
           initial_state_fw=None,
@@ -40,7 +41,7 @@ def blstm(inputs,
         cell_fw=fw_cell,
         cell_bw=bw_cell,
         inputs=inputs,
-        sequence_length=seq_len,
+        sequence_length=input_sequence_length,
         initial_state_fw=initial_state_fw,
         initial_state_bw=initial_state_bw,
         dtype=tf.float32,
@@ -63,42 +64,47 @@ def reshape_pyramidal(outputs, sequence_length):
 
 def Model(inputs,
     seq_len,
+    input_sequence_length,
+    char_ids,
+    maximum_iterations,
     num_classes=28,
     num_hidden = 100,
     num_layers = 1,
     batch_size = 1,
     is_training=True,
+    num_encoder_layers = 1,
+    num_decoder_layers = 1,
     scope='model_attention'):
     with tf.variable_scope(scope, 'model_attention', [inputs, seq_len]) as sc:
         # Encoder
-        encoder_outputs, encoder_state = build_encoder(inputs, seq_len)
+        encoder_outputs, encoder_state = build_encoder(inputs, seq_len, num_encoder_layers)
 
         # Decoder
-        logits, _, _ = build_decoder(encoder_outputs,encoder_state,seq_len, num_hidden, num_layers)            
+        logits, _, _ = build_decoder(encoder_outputs,encoder_state,num_classes, num_decoder_layers, maximum_iterations)            
         return logits
 
-def build_encoder(inputs, seq_len):
+def build_encoder(inputs, seq_len, input_sequence_length, num_encoder_layers):
     
     # Pyramidal bidirectional LSTM(s)
-    #inputs = audios
-    #seq_lengths = audio_sequence_lengths
 
     initial_state_fw = None
     initial_state_bw = None
 
-    for n in range(1):
+    global rnn_size_encoder
+
+    for n in range(num_encoder_layers):
         scope = 'pBLSTM' + str(n)
         (out_fw, out_bw), (state_fw, state_bw) = blstm(
             inputs,
-            15000,
-            256,
+            input_sequence_length,
+            rnn_size_encoder // 2,
             scope=scope,
             initial_state_fw=initial_state_fw,
             initial_state_bw=initial_state_bw
         )
 
         inputs = tf.concat([out_fw, out_bw], -1)
-        inputs, seq_len = reshape_pyramidal(inputs, seq_len)
+        inputs, input_sequence_length = reshape_pyramidal(inputs, input_sequence_length)
         initial_state_fw = state_fw
         initial_state_bw = state_bw
 
@@ -106,16 +112,15 @@ def build_encoder(inputs, seq_len):
     bi_state_c = tf.concat((initial_state_fw.c, initial_state_fw.c), -1)
     bi_state_h = tf.concat((initial_state_fw.h, initial_state_fw.h), -1)
     bi_lstm_state = tf.nn.rnn_cell.LSTMStateTuple(c=bi_state_c, h=bi_state_h)
-    encoder_state = tuple([bi_lstm_state] * 1)
+    encoder_state = tuple([bi_lstm_state] * num_encoder_layers)
 
     return inputs, encoder_state
 
 
 
-def build_decoder(encoder_outputs, encoder_state, seq_len, num_hidden, num_layers):
+def build_decoder(encoder_outputs, encoder_state, num_classes, num_decoder_layers,maximum_iterations):
 
-    eos_id_2 = tf.cast(29, tf.int32)
-    output_layer = Dense(30, name='output_projection')
+    out_layer = Dense(num_classes+1, name='output_projection')
 
     # Decoder.
     with tf.variable_scope("decoder") as decoder_scope:
@@ -123,9 +128,9 @@ def build_decoder(encoder_outputs, encoder_state, seq_len, num_hidden, num_layer
         cell, decoder_initial_state = build_decoder_cell(
             encoder_outputs,
             encoder_state,
-            seq_len,
-            num_hidden,
-            num_layers) #audio sequence length
+            input_sequence_length,
+            num_decoder_layers,
+            batch_size)
 
         # Train
         # if mode != 'INFER':
@@ -133,15 +138,20 @@ def build_decoder(encoder_outputs, encoder_state, seq_len, num_hidden, num_layer
                                        shape=[None, None],
                                        name='ids_target')
         embedding = tf.get_variable('embedding',
-                                    shape=[30+1, 300],
+                                    shape=[num_classes+1, 2], # embeddings dimension I have given 2
                                     dtype=tf.float32)
 
-        char_embedding = tf.nn.embedding_lookup(embedding,
+
+        char_embedding_lookup = tf.nn.embedding_lookup(embedding,
                                                 char_ids,
                                                 name='char_embedding')
+        char_embedding = tf.nn.dropout(char_embedding_lookup,
+                                            0.986,
+                                            name='char_embedding_dropout')
+
         helper = tf.contrib.seq2seq.ScheduledEmbeddingTrainingHelper(
             inputs=char_embedding,
-            sequence_length=seq_len,
+            sequence_length=num_classes,
             embedding=embedding,
             sampling_probability=0.5,
             time_major=False)
@@ -150,13 +160,13 @@ def build_decoder(encoder_outputs, encoder_state, seq_len, num_hidden, num_layer
         my_decoder = tf.contrib.seq2seq.BasicDecoder(cell,
                                                      helper,
                                                      decoder_initial_state,
-                                                     output_layer=output_layer)
+                                                     output_layer=out_layer)
 
         # Dynamic decoding
         outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
             my_decoder,
             output_time_major=False,
-            maximum_iterations=800,
+            maximum_iterations=maximum_iterations,
             swap_memory=False,
             impute_finished=True,
             scope=decoder_scope
@@ -217,13 +227,13 @@ def build_decoder(encoder_outputs, encoder_state, seq_len, num_hidden, num_layer
     return logits, sample_id, final_context_state
 
 def build_decoder_cell(encoder_outputs, encoder_state,
-                       audio_sequence_lengths, num_hidden, num_layers):
+                       input_sequence_length, num_decoder_layers, batch_size):
     """Builds the attention decoder cell. If mode is inference performs tiling
        Passes last encoder state.
     """
 
     memory = encoder_outputs
-
+    global rnn_size_decoder
     # if mode == 'INFER' and beam_width > 0:
     #     memory = tf.contrib.seq2seq.tile_batch(memory,
     #                                            multiplier=beam_width)
@@ -244,13 +254,13 @@ def build_decoder_cell(encoder_outputs, encoder_state,
 
     # attention cell
     lstm_cell = tf.nn.rnn_cell.MultiRNNCell(
-                [make_rnn_cell(512) for _ in
-                 range(1)])
+                [make_rnn_cell(rnn_size_decoder) for _ in
+                 range(num_decoder_layers)])
     cell = make_attention_cell(lstm_cell,
-                                    512,
+                                    rnn_size_decoder,
                                     memory,
-                                    audio_sequence_lengths) 
+                                    input_sequence_length) 
 
-    decoder_initial_state = cell.zero_state(1, tf.float32).clone(cell_state=encoder_state)
+    decoder_initial_state = cell.zero_state(batch_size, tf.float32).clone(cell_state=encoder_state)
 
     return cell, decoder_initial_state
